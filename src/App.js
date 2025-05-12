94410d4aee779a8f7ac7ef267358f586dd019a88
vvalmir-silva <vvalmir.devsilva@gmail.com>
2025-05-12 17:20:21 -0300

    updateapp.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
+import imagemBody from "./img/capa.jpg";
+
+const HeaderComponents = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const HeaderDiv = styled.div`
+  text-align: center;
+`;
+
+const HeaderInput = styled.input`
+  font-size: 20px;
+  display: flex;
+  border-radius: 10px;
+  align-items: center;
+`;
+const BuscarCepH1 = styled.h1`
+  font-family: sans-serif;
+  cursor: pointer;
+  color: white;
+`;
+
+const UlConpondentsCompondents = styled.h1`
+  color: white;
+  font-size: 26px;
+`;
 
 function App() {
+  const [endereco, setEndereco] = useState("");
+
+  function manipularEndereco(event) {
+    const cep = event.target.value;
+
+    setEndereco({
+      cep,
+    });
+    if (cep && cep.length === 8) {
+      //obter o cep
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then((resposta) => resposta.json())
+        .then((dados) => {
+          setEndereco((enderecoAntigo) => {
+            return {
+              ...enderecoAntigo,
+              rua: dados.logradouro,
+              bairro: dados.bairro,
+              cidade: dados.estado,
+              estado: dados.estado,
+              regiao: dados.regiao,
+              ibge: dados.ibge,
+              ddd: dados.ddd,
+            };
+          });
+        });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HeaderDiv>
+      <HeaderComponents>
+        <div>
+          <img src={imagemBody} alt="Minha imagem" />
+        </div>
+        <BuscarCepH1>BUSCAR CEP</BuscarCepH1>
+        <HeaderInput
+          onChange={manipularEndereco}
+          className="cep"
+          placeholder="Digite seu cep"
+        />
+
+        <ul>
+          <UlConpondentsCompondents>
+            <li>Cep: {endereco.cep}</li>
+            <li>Rua: {endereco.rua}</li>
+            <li>Bairro: {endereco.bairro}</li>
+            <li>Cidade: {endereco.cidade}</li>
+            <li>Estado: {endereco.estado}</li>
+            <li>Região: {endereco.regiao}</li>
+            <li>Ibge: {endereco.ibge}</li>
+            <li>DDD: {endereco.ddd}</li>
+          </UlConpondentsCompondents>
+        </ul>
+      </HeaderComponents>
+    </HeaderDiv>
   );
 }
 
